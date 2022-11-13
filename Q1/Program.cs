eefa1aaa97e5a5f70b0fcd2e7aa7769917369371
User <User@DESKTOP-M776RP5>
2022-11-13 14:48:02 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			IEnumerable<int> list = Enumerable.Range(1,10);
+			foreach(int i in list)
+			{
+				Console.WriteLine(i);
+			}
+		}
+	}
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			IEnumerable<int> list = Enumerable.Range(10, 6);
+			foreach(int i in list)
+			{
+				Console.WriteLine(i);
+			}
+		}
+	}
+}
